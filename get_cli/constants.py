a7322d8d1695a8752891cd01a3f3c30aed5d23da
noamtamir <noam.tamir@gmail.com>
2023-03-22 15:00:54 +0100

    remove comments

--- get_cli/constants.py
+++ get_cli/constants.py
@@ -1,5 +1,6 @@
+import os
 from pathlib import Path
 
-DEFAULT_EDITOR = 'vim'  # TODO: os.getenv('EDITOR', 'vim')
+DEFAULT_EDITOR = os.getenv('EDITOR', 'vim')
 DEFAULT_STORAGE_PATH = Path.home() / '.get.yml'
 DEFAULT_STORAGE_DATA = {'hello': 'world'}
